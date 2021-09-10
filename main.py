70d847b8b68c67b8efe662a778216c8021739195
CodewithAditya22 <90127952+CodewithAditya22@users.noreply.github.com>
2021-09-10 11:58:43 +0530

    added function to show dalchadr value
    made a function that tells value if user enters dal chadar as skuid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import openpyxl
-from dict import ts
+from dict import ts,dal_chadar
 filename = "2020-06-14 - STOCK UPDATE.xlsx"
 
 wb = openpyxl.load_workbook(filename)
@@ -14,10 +14,20 @@ def pri_data_TSHIRT():
     SIZE_TSHIRT = input("Enter Size: ")
     data_TSHIRT = sh1[ts[SKUID_TSHIRT][SIZE_TSHIRT]].value
     print(data_TSHIRT)
+def pri_data_DAL_CHADAR():
+    """
+    FUNCTION TO PRINT VALUE IF USER
+        ENTERS DAL CHADAR AS SKUID
+    """
+    SKUID_DAL_CHADAR = input("Enter SKUID")
+    data_dalchadar = sh1[dal_chadar[SKUID_DAL_CHADAR]].value
+    print(data_dalchadar)
+
 #---------------------------------------------------------------
 User_sheet_input = input("Enter Sheet name: ")
 sh1 = wb[User_sheet_input]
 if User_sheet_input == "T-SHIRT":
     pri_data_TSHIRT()
-
+if User_sheet_input == "DAL CHADAR":
+    pri_data_DAL_CHADAR()
 
